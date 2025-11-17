6faa632fe992932c4abef3a4f7a8912bd978a700
husam <husam.donus@gmail.com>
2025-11-17 22:41:18 +0300

    Continue....

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-blue-500 text-4xl">Hello</Text>
+      <Link href="/explore">expolre</Link>
+      <Link href="/profile">profile</Link>
+      <Link href="/properties/1">property 1</Link>
+      <Link href="/sign-in">sign in</Link>
+    </View>
+  );
+}
